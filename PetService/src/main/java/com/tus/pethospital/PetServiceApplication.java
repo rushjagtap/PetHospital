15caab18e75cb025f9b8c078eb3d4f9b8b74cf1c
rushjagtap <rushikeshjagtap0@gmail.com>
2022-04-16 19:35:09 +0100

    config setup

--- PetService/src/main/java/com/tus/pethospital/PetServiceApplication.java
+++ PetService/src/main/java/com/tus/pethospital/PetServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class PetServiceApplication {
 
 	public static void main(String[] args) {
